04ea65435247c6f96d0d22c341949e44557d51cd
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2024-05-16 18:10:09 +0600

    aaa

--- scripts/ai.ts
+++ scripts/ai.ts
@@ -86,6 +86,10 @@ async function claudeAiRequest() {
   });
 }
 
-claudeAiRequest();
-openAiRequest();
-// viewLastResult();
+async function main() {
+  claudeAiRequest();
+  openAiRequest();
+  // viewLastResult();
+}
+
+main();
